56608175fc1e4d259edc5f65d4b99b34448c4dd4
=ahmed <=amohammed147258@gmail.com>
2024-10-16 18:35:38 +0300

    I make changes in my code

---
+++ Products/index.js
@@ -0,0 +1,31 @@
+let see = document.getElementById("seeAll");
+let seeTwo = document.getElementById("see");
+let seeThree = document.getElementById("seee");
+let seeW = document.getElementById("seeW");
+let seeB = document.getElementById("seeB");
+let boxes = document.getElementById("boxes");
+let boxess = document.getElementById("boxess");
+let boxesss = document.getElementById("boxesss");
+let boxesWatch = document.getElementById("boxesWatch");
+let boxesBag = document.getElementById("boxesBag");
+
+see.addEventListener("click", function () {
+  boxes.style.height = "fit-content";
+  // see.style.display = "none";
+});
+seeTwo.addEventListener("click", function () {
+  boxess.style.height = "fit-content";
+  // see.style.display = "none";
+});
+seeThree.addEventListener("click", function () {
+  boxesss.style.height = "fit-content";
+  // see.style.display = "none";
+});
+seeW.addEventListener("click", function () {
+  boxesWatch.style.height = "fit-content";
+  // see.style.display = "none";
+});
+seeB.addEventListener("click", function () {
+  boxesBag.style.height = "fit-content";
+  // see.style.display = "none";
+});
